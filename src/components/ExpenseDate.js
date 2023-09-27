39a483af6a14f38865854ac048b3ffaf64e162c4
Jürmo Mihhailov <mihhailovjurmo@gmail.com>
2023-09-27 16:35:41 +0300

    ylesanne 1

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,4 +1,4 @@
-import './ExpenseItem.css';
+import './ExpenseDate.css';
 
 const ExpenseDate = (props) => {
     const day = props.date.toLocaleString('en-US', {day: '2-digit'});
@@ -6,10 +6,10 @@ const ExpenseDate = (props) => {
     const year = props.date.getFullYear();
 
     return (
-        <div>
-            <div>{month}</div>
-            <div>{day}</div>
-            <div>{year}</div>
+        <div className="expense-date">
+            <div className='expense-date__month'>{month}</div>
+            <div className='expense-date__year'>{day}</div>
+            <div className='expense-date__day'>{year}</div>
         </div>
     )
 }

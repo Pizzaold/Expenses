7f1c336a7f55d8c795740e44486990a332249998
Jürmo Mihhailov <mihhailovjurmo@gmail.com>
2023-09-27 16:29:13 +0300

    exercise 4.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 const Expenseitem = (props) => {
-    console.log(props)
+    const day = props.expenseData.date.toLocaleString('en-US', {day: '2-digit'});
+    const month = props.expenseData.date.toLocaleString('en-US', {month: 'long'});
+    const year = props.expenseData.date.getFullYear();
 
     return (
         <div className='expense-item'>
-            <div>{props.expenseData.date.toString()}</div>
+            <ExpenseDate date={props.expenseData.date}></ExpenseDate>
             <div className='expense-item__description'>
                 <h2>{props.expenseData.title}</h2>
                 <div className='expense-item__price'>{props.expenseData.price}</div>

f278571a353da30b2eceeb6975bb90e1a18e5c59
Pizzaold <109032167+Pizzaold@users.noreply.github.com>
2023-10-02 15:25:11 +0300

    exercise 6

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,14 +1,15 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 
 const Expenses = (props) => {
     console.log(props);
     return (
-        <div className='expenses'>
+        <Card className='expenses'>
             {props.expenseData.map((expense, index) => (
                 <ExpenseItem key={index} data={expense}></ExpenseItem>
             ))}
-        </div>
+        </Card>
     );
 }
 

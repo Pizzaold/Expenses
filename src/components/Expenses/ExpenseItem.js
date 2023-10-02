d9a8c1376e2f2c953e81259fb92211e6b75dfe0e
Jürmo Mihhailov <mihhailovjurmo@gmail.com>
2023-10-02 17:10:32 +0300

    E

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -13,7 +13,7 @@ const Expenseitem = (props) => {
         <div className='expense-item'>
             <ExpenseDate date={props.data.date}></ExpenseDate>
             <div className='expense-item__description'>
-                <h2>{props.data.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>{props.data.price}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>

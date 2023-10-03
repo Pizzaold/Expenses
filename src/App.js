0eee7a74358a563466f2d2fdb67d83abf14d05b9
Pizzaold <109032167+Pizzaold@users.noreply.github.com>
2023-10-03 19:05:52 +0300

    Exersice 8

--- src/App.js
+++ src/App.js
@@ -3,28 +3,16 @@ import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
-  const expense = [
-    {
-    date: new Date(2023, 0, 10),
-    title: 'New book',
-    price: 30.99,
-    },
-    {
-    date: new Date(2022, 2, 15),
-    title: 'New TV',
-    price: 100.99,
-    },
-    { 
-    date: new Date(2023, 10, 2),
-    title: 'New PC',
-    price: 2000,
-    },
-  ]
+  const expense = []
   
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js')
+    console.log(expense)
+  }
 
   return (
     <div className='App'>
-      <NewExpense></NewExpense>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
       <Expenses expenseData={expense}></Expenses>
     </div>
   );

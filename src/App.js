3619da0aa1457adfc0f7d6c11a1145a77cf1e805
Pizzaold <109032167+Pizzaold@users.noreply.github.com>
2023-10-03 19:27:36 +0300

    Ylesanne 3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
-  const expense = []
+  const expense = [
+    {
+    date: new Date(2023, 0, 10),
+    title: 'New book',
+    price: 30.99,
+    },
+    {
+    date: new Date(2022, 2, 15),
+    title: 'New TV',
+    price: 100.99,
+    },
+    { 
+    date: new Date(2023, 10, 2),
+    title: 'New PC',
+    price: 2000,
+    },
+  ]
   
   const addExpenseHandler = (expense) => {
     console.log('In App.js')
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,25 @@
+import React, { useState } from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
-    console.log(props);
+    const [filteredYear, setFilteredYear] = useState('');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = props.expenseData.filter((expense) => {
+        return filteredYear === '' || expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return (
         <Card className='expenses'>
-            {props.expenseData.map((expense, index) => (
-                <ExpenseItem key={index} data={expense}></ExpenseItem>
+            <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+            {filteredExpenses.map((expense, index) => (
+                <ExpenseItem key={index} data={expense} />
             ))}
         </Card>
     );

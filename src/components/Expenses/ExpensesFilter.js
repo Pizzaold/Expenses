3b17a2794a3f438e693cf4a6ffe45bb2f3178fbf
Jürmo Mihhailov <mihhailovjurmo@gmail.com>
2023-10-06 13:11:49 +0300

    Add all filter

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -16,6 +16,7 @@ const ExpensesFilter = (props) => {
             <div className='expenses-filter__control'>
                 <label>Filter by year</label>
                 <select value={selectedYear} onChange={yearChangeHandler}>
+                    <option value=''>All</option>
                     <option value='2022'>2022</option>
                     <option value='2023'>2023</option>
                     <option value='2024'>2024</option>

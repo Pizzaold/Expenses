32a7a8cc17c881c2941445c63c8ec383593ede12
Pizzaold <109032167+Pizzaold@users.noreply.github.com>
2023-11-02 15:56:26 +0200

    E

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -61,7 +61,7 @@ const ExpenseForm = (props) => {
                     <label>Date</label>
                     <input
                         type="date"
-                        min="2023-01-01"
+                        min="2020-01-01"
                         max="2025-12-31"
                         onChange={dateChangeHandler}
                         value={enteredDate}
